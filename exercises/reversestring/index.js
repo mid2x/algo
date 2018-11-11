6c498c8d76f0be46347cff7050152f467561fbc6
mid2x <hamid.shebubakar@gmail.com>
2018-11-11 11:30:11 +0700

    hai

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,20 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  n = str.length;
+  var rev = '';
+  for (var i=(n-1);i>=0;i--){
+// for (let character of str){
+// rev = character + rev;
+// }
+    rev = rev+str[i];
+  }
+  return rev;
+}
 
 module.exports = reverse;
+
+// function reverse(str) {
+//   return str.split('').reverse().join('');
+// }
