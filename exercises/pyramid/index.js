7a4a379b5a8fda2f4f0327ff55fb1b418979c31a
mid2x <hamid.shebubakar@gmail.com>
2018-12-16 22:20:14 +0700

    recurssion on pyramid

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -13,7 +13,35 @@
 //       '  #  '
 //       ' ### '
 //       '#####'
-
-function pyramid(n) {}
+function pyramid(n) {
+  for (i=1;i<=n;i++){
+    tri = '';
+    a = (n*2)-1;
+    for (j=1;j<=a;j++){
+      if ((j>(n-i))&&(j<(n+i))){
+        tri += '#';
+      }else{
+        tri += ' ';
+      }
+    }
+    console.log(tri);
+  }
+}
+// recursive solution
+// function pyramid(n, row=0, tri='') {
+//   if (row === n){
+//   return;
+//   }
+//   if (tri.length === 2 * n - 1){
+//     console.log(tri);
+//     return pyramid (n, row + 1);
+//   }
+//   if (n-1 - row <= tri.length && n-1 + row >= tri.length){
+//     tri += '#';
+//   }else{
+//     tri += ' ';
+//   }
+//   pyramid (n, row, tri)
+// }
 
 module.exports = pyramid;
