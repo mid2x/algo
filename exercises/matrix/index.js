6c7e69280b480298efa9bde621fffb06f4c9b039
mid2x <hamid.shebubakar@gmail.com>
2018-12-25 17:03:58 +0700

    liburan

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,40 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+hasil =[];
+for (i=0;i<n;i++){
+  hasil[i]= new Array(n);
+  }
+  km = 0;
+  ks = n-1;
+  bm = 0;
+  bs = n-1;
+  cnt = 1;
+
+  while (cnt <= n*n){
+    for (i = km; i<=ks;i++){
+      hasil[bm][i] = cnt;
+      cnt++;
+    }
+    bm++;
+    for (i = bm; i<=bs; i++){
+      hasil[i][bs] = cnt;
+      cnt++;
+    }
+    ks--;
+    for (i = ks; i>=km; i--){
+      hasil[bs][i] = cnt;
+      cnt++
+    }
+    bs--;
+    for (i=bs; i>=bm; i--){
+      hasil[i][km] = cnt;
+      cnt++;
+    }
+    km++;
+  }
+  return hasil;
+}
 
 module.exports = matrix;
--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,30 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
-
+function vowels(str) {
+    count = str.length;
+    nei = str.replace(/[aiueo]/gi, '').length;
+     // console.log(count);
+     // console.log(nei);
+    return count - nei;
+  }
 module.exports = vowels;
+
+//  alternate solution:
+// function vowels(str) {
+//     count = 0;
+//     check = 'aiueo';
+//     for (a of str.toLowerCase()){
+//       if (check.includes(a)){
+//         count++;
+//       }
+//     }
+//     return count;
+//   }
+//  another using regular explession  
+// function vowels(str) {
+//     matches = str.match(/[aiueo]/ig);
+//     return matches ? matches.length : 0;
+//   }
+//
+//   vowels('bunda kandung');
