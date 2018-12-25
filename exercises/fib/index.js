7e0ae13af51940861a45f1b4061f941532ace4dc
mid2x <hamid.shebubakar@gmail.com>
2018-12-25 23:07:49 +0700

    reurssive

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,29 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+function fib(n) {
+  f = [0,1];
+    for(i=2; i<=n; i++){
+      f[i] = f[i-1]+f[i-2];
+    }return f[n];
+  }
+
+//  recursive by using the above
+//   function fib(n, f = [0,1], i=2) {
+//   if (i<=n){
+//   f[i] = f[i-1]+f[i-2];
+//   i++;
+//   fib(n,f,i);
+//   }
+//   return f[n];
+// }
+
+//  more recursive solution but very delicate with imangination
+// function fib(n){
+//   if(n<2) {
+//     return n;
+//   }
+//   return fib(n-1)+fib(n-2);
+// }
 
 module.exports = fib;
